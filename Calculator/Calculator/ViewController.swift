e7a39ce8de903a363f12fa6bcd0cf6847bc001ed
Jevin Anderson <jevinanderson@gmail.com>
2017-08-11 10:08:10 -0700

    Initial Calculator commit

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Anderson, Jevin on 8/11/17.
+//  Copyright © 2017 Anderson, Jevin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
